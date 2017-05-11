29bf295fb9124859f87b83ced818ccfd58891876
Gianni <gianni.freddie@gmail.com>
2017-05-11 11:24:04 +0200

    Layout generale app più o meno ok

--- feeds.php
+++ feeds.php
@@ -17,13 +17,20 @@
         <div class="navbar-header">
            <a class="navbar-brand" href="#"><img src = "/tweb_progetto_finale/images/site-icon.png"/><h1>Nerd organizer</h1></a>
         </div>
-        <div class = "collapse navbar-collapse pull-right">
+        <div class = "collapse navbar-collapse navbar-right">
           <ul class = "nav navbar-nav">
             <li class = "active"><a href="#">Home</a></li>
             <li><a href="#">Tv series</a></li>
             <li><a href="#">Games</a></li>
             <li><a href="#">Movies</a></li>
             <li><a href="#">Comics</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="/tweb_progetto_finale/logout.php">Logout</a></li>
+                <li><a href="#">Edit profile</a></li>
+              </ul>
+            </li>
           </ul>
         </div>
       </div>
@@ -42,14 +49,69 @@
         ?>
         <h3>@<?= $user["nickname"] ?></h3>
         <div class="row">
-          <div class="col-md-3 text-right ">
+          <div class="col-md-3 ">
             <p>Name:</p>
           </div>
           <div class="col-md-6">
+            <? if($user["name"] != null) { ?>
+              <p><?= ucfirst($user["name"]) ?></p>
+            <? }else{ ?>
+              <p> - </p>
+            <? }?>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 ">
+            <p>Surname:</p>
+          </div>
+          <div class="col-md-6">
+            <? if($user["surname"] != null) { ?>
+              <p><?= $user["surname"] ?></p>
+            <? }else{ ?>
+                <p> - </p>
+            <? } ?>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 ">
+            <p>School:</p>
+          </div>
+          <div class="col-md-6">
+            <? if($user["school"] != null) { ?>
+              <p><<?= $user["school"] ?></p>
+            <? }else{ ?>
+              <p> - </p>
+            <? } ?>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 ">
+            <p>Job:</p>
+          </div>
+          <div class="col-md-6">
+            <? if($user["job"] != null) { ?>
+              <p><<?= $user["job"] ?></p>
+            <? }else{ ?>
+              <p> - </p>
+            <? } ?>
+          </div>
+        </div>
+      </div>
+      <!-- div ore spese a fare cose -->
+      <div class="time-info">
+        <div class="row">
+          <div class = "col-md-8 ">
+            <p>Hours spent watching tv series:</p>
+          </div>
+          <div class="col-md-4">
             <p> - </p>
           </div>
         </div>
       </div>
     </div>
+    <script src="/tweb_progetto_finale/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/tweb_progetto_finale/bootstrap/js/bootstrap.js"></script>
   </body>
 </html>
