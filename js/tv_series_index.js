0b4e70fa98332c88504fd1f59a8439cda52018e5
Gianni <gianni.freddie@gmail.com>
2017-08-01 23:57:31 +0200

    Capire perché non posso usare jQuery anche se c’è

--- js/tv_series_index.js
+++ js/tv_series_index.js
@@ -1,6 +1,8 @@
 document.observe("dom:loaded", function() {
-	$(".telefilm-info").click(function(){ //gestisci click su scheda telefilm, deve aprire pagina php del film
-		var telefilm_id = $(this).data("tv-serie-id");
-		console.log("Bravo hai cliccato sul teleflm con id " + telefilm_id);
-	});
+	$(".telefilm-info").each(function())
 });
+
+//click handler
+function goToSerieShow(event) {
+	console.log("Ciao");
+}
